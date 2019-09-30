d34e1fb49e623279d588976589fd8e8e00a61741
ayatsyny <andriy.yatsynyak@gmail.com>
2019-09-30 18:58:14 +0300

    init example

---
+++ example_2.py
@@ -0,0 +1,46 @@
+import queue
+from lib.elapsed_time import ET
+
+
+def task(name, queue):
+    while not queue.empty():
+        count = queue.get()
+        total = 0
+        print(f"Task {name} running")
+        for x in range(count):
+            total += 1
+            yield
+        print(f"Task {name} total: {total} return {count}")
+
+
+def main():
+    """
+    This is the main entry point for the program.
+    """
+    # Create the queue of 'work'
+    work_queue = queue.Queue()
+    # Put some 'work' in the queue
+    for work in [10, 15, 6, 2]:
+        work_queue.put(work)
+    # Create some tasks
+    tasks = [
+        task("One", work_queue),
+        task("Two", work_queue)
+    ]
+    # Run the tasks
+    et = ET()
+    done = False
+    while not done:
+        for t in tasks:
+            try:
+                next(t)
+            except StopIteration:
+                tasks.remove(t)
+            if len(tasks) == 0:
+                done = True
+
+    print(f"\nTotal elapsed time: {et():.1f}")
+
+
+if __name__ == "__main__":
+    main()
